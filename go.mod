a7344516cd2eab808f06c68968406cd0441f979b
Shahfiq Shah <shahfiq.dev@gmail.com>
2023-10-08 21:30:43 +0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gr3gg0r/bmi
+
+go 1.21.2
---
+++ info/info.go
@@ -0,0 +1,6 @@
+package info
+
+const MainTitle = "BMI Calculator"
+const Seperator = "------------------------"
+const WeightPrompt = "Please enter your weight (kg): "
+const HeightPrompt = "Please enter your height (m): "
